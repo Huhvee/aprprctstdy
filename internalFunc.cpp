d15f42c0650b104185cdc641974e68270e91a227
Huhvee <mike20081980@gmail.com>
2025-12-06 21:47:13 +0200

    fixed :)

--- internalFunc.cpp
+++ internalFunc.cpp
@@ -1,24 +1,27 @@
 #include "dependencies.h"
 
+//rewrite allthat
+
 void aprox() {
 	double value = 0,
-		valuex=0,
-		step = 0.1,
-		prev = -0.1,
+		valuex = 0,
+		step = 1,
+		prev = -1,
 		current = 0;
 	while (true) {
-		valuex = func1(prev) - func2(prev);
-		value = func1(current) - func2(current);
-		if (value == 0) break;
-		if (valuex < value) {
-			step = -step;
-			step *= step;
+		valuex = abs(func1(prev) - func2(prev));
+		value = abs(func1(current) - func2(current));
+
+		if (round_acc(value, accuracy) == 0) {
+			break;
 		}
-		else {
-			prev = current;
-			current += step;
+
+		if (value > valuex) {
+			step = -step * 0.1;
 		}
-		std::cout << current << '\n';
+		prev = current;
+		current += step;
+		std::cout << current<<'\n';
 	}
-	std::cout << round_acc(current, accuracy);
+	std::cout << current << '\n';
 }
